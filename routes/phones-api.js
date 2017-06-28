d6ae307daa3442192c3186ffc27ad31a32d4806f
Adel Acebal <adelacebal@yahoo.com>
2017-06-28 15:58:54 -0400

    first commit

---
+++ routes/phones-api.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+const Phone = require('../models/phone-model');
+
+router.get('/phones', (req, res, next) => {
+  Phone.find((err, phonesList) => {
+    if(err) {
+      res.json(err);
+      return;
+    }
+    res.json(phonesList);
+  });
+});
+
+router.post('/phones', (req, res, next) => {
+  const thePhone = new Phone({
+    brand: req.body.brand,
+    name: req. body.name,
+    specs: req.body.specs,
+    image: req.body.image || ''
+  });
+
+  thePhone.save((err) => {
+    if (err) {
+      res.json(err);
+      return;
+    }
+    
+    res.json({
+      message: 'New Phone created!',
+      id: thePhone._id
+    });
+  });
+});
+
+module.exports = router;
